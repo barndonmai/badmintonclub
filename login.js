04c79e584a34371cab2999cf0ebd538ea7862d63
Kevin Kim <115194104+yjk620@users.noreply.github.com>
2024-04-02 00:33:01 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,41 @@
+function userType(event) {
+  event.preventDefault();
+
+  const username = document.getElementById('username');
+  const password = document.getElementById('password');
+  const memberArr = [['kevinkim', 'aslakdj']];
+  const coachArr = [
+    ['janepear', 'wpoeri'],
+    ['jerrylee', 'zxmbak'],
+  ];
+  const treasurerArr = [['john', 'bgebui']];
+  const staffArr = [['michaelorange', 'xrdvgbh']];
+
+  if (
+    memberArr.some(
+      ([user, pass]) => user === username.value && pass === password.value
+    )
+  ) {
+    window.location.href = 'member.html';
+  } else if (
+    coachArr.some(
+      ([user, pass]) => user === username.value && pass === password.value
+    )
+  ) {
+    window.location.href = 'coach.html';
+  } else if (
+    treasurerArr.some(
+      ([user, pass]) => user === username.value && pass === password.value
+    )
+  ) {
+    window.location.href = 'treasurer.html';
+  } else if (
+    staffArr.some(
+      ([user, pass]) => user === username.value && pass === password.value
+    )
+  ) {
+    window.location.href = 'staff.html';
+  } else {
+    alert('Invalid username or password!');
+  }
+}
